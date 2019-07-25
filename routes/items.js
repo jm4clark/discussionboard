9b1cb6e1cb03968dc197a9abf7ed0b96c3d163eb
jm4clark <jamesmclark96@gmail.com>
2019-07-25 15:07:35 +0100

    some hashing, needs improving

--- routes/items.js
+++ routes/items.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const validator = require("validator");
-const isEmpty = require("../validation/is-empty.js");
-
+const bcrypt = require("bcrypt");
 const Item = require("../models/items.js");
+const validateItem = require("../validation/validateItem.js");
 
 router.get("/getAll", (req, res) => {
     const errors = {};
@@ -20,18 +19,52 @@ router.get("/getAll", (req, res) => {
 
 router.post("/add", (req, res) => {
     var r = req.body;
-    if(!isEmpty(r)){
-        const errors = {};
-        var newItem = new Item({
-            name: r.name,
-            content: r.content,
-            email: r.email
-        });
-
-        newItem.save().then(() => console.log("added"));
-        res.end("end");
+    const validate = validateItem(r);
+    if(!validate.isValid){
+        return res.status(400).json(validate.errors);
     }
+    const errors = {};
+    var newItem = new Item({
+        username: r.username,
+        content: r.content,
+        email: r.email
+    });
+
+    newItem.save().then(() => console.log("added")
+    .catch(err => res.status(404).json(err)));
+    res.end("item added!");
+    
 })
+
+router.post("/addHash", (req, res) => {
+
+    var r = req.body;
+    const validate = validateItem(r);
+    if(!validate.isValid){
+        return res.status(400).json(validate.errors);
+    }
+    const errors = {};
+    var newItem = new Item({
+        username: r.username,
+        content: r.content,
+        email: r.email
+    });
+    
+    console.log("begin hashing");
+    payload = {};
+    bcrypt.genSalt(10, (err,salt) => {
+        console.log("salted");
+        bcrypt.hash(newItem.email, salt, (err,hash) => {
+            console.log("hashed");
+            if (err) { console.log(err); }
+            newItem.email = hash;
+            payload.value = hash;
+            newItem.save().then(item => res.json(Item))
+            .catch(err => console.log(err));
+            res.json(payload);
+        });
+    });
+});
 
 router.put("/update", (req, res) => {
     r = req.body;
